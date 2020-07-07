b43a8c220dbf99f1b66e914f5f6081b2063a8541
Mohit <48782578+mohito1999@users.noreply.github.com>
2020-07-06 21:53:39 -0600

    Added script and .exe file via upload.

---
+++ app.py
@@ -0,0 +1,99 @@
+import tkinter as tk
+from bs4 import BeautifulSoup
+import requests
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import sys
+import time
+from twisted.internet.task import LoopingCall
+from twisted.internet import reactor
+import threading
+
+
+class App(tk.Frame):
+    def __init__(self, title, master=None):
+        tk.Frame.__init__(self, master)
+        self.pack()
+        self.master.title
+
+        self.class_id = tk.StringVar()
+        self.email = tk.StringVar()
+        self.password = tk.StringVar()
+
+        tk.Label(self, text="Enter your class URL here").grid(column=0, row=0)
+        tk.Entry(self, textvariable=self.class_id).grid(column = 1, row=0)
+        tk.Label(self, text="Enter your email address here").grid(column=0, row=1)
+        tk.Entry(self, textvariable=self.email).grid(column = 1, row=1)
+        tk.Label(self, text="Enter your password here").grid(column=0, row=2)
+        tk.Entry(self, textvariable=self.password, show="*").grid(column = 1, row=2)
+        #tk.Label(self, text="Please keep this window open for regular checks.\nClose window when you have registered in desired class.").grid(column=0, row=4)
+
+        tk.Button(self, text="Initialize Details", command=self.enter_var).grid(column=0, row=3)
+        tk.Button(self, text="Begin Monitoring Class", command=self.interval).grid(column=1, row=3)
+        
+
+    def enter_var(self):
+        self.class_entry = self.class_id.get()
+        self.email_entry = self.email.get()
+        self.password_entry = self.password.get()
+
+        
+    
+    def send_email(self):
+        while True:
+            url = requests.get(self.class_entry)
+            html_str = url.text
+            full_not = BeautifulSoup(html_str, 'html.parser')
+            tr_tag = full_not.find_all('tr')
+            tr_tags = []
+            for tag in tr_tag:
+                tr_tags.append(tag.get_text())
+            for i in range(len(tr_tags)):
+                if "Total Seats Remaining" in tr_tags[i]:
+                    rel_str = tr_tags[i]
+            for i in range(len(rel_str)):
+                if rel_str[i] == ':' and rel_str[i + 1] == "0":
+                    seats_remaining = 0
+                    seats_remaining
+                elif rel_str[i] == ':' and int(rel_str[i + 1]) > 0:
+                    seats_remaining = 1
+                    seats_remaining
+            fromaddr = self.email_entry
+            toaddr = self.email_entry
+            msg = MIMEMultipart()
+            msg['From'] = fromaddr
+            msg['To'] = toaddr
+            msg['Subject'] = "A Seat Has Opened Up!"
+            body = "Go To " + self.class_entry
+            msg.attach(MIMEText(body, 'plain'))
+            server = smtplib.SMTP('smtp.gmail.com', 587)
+            server.starttls()
+            server.login(fromaddr, self.password_entry)
+            text = msg.as_string()
+            if seats_remaining == 1:
+                server.sendmail(fromaddr, toaddr, text)
+                server.quit()
+            time.sleep(60)
+            continue
+        
+
+    def interval(self):
+        t = threading.Thread(target=self.send_email)
+        t.daemon = True #ends thread when there are no other non-thread scripts active
+        t.start()
+        
+            
+            
+        
+        '''
+        ls = LoopingCall(self.send_email)
+        ls.start(10)
+        reactor.run()
+        '''
+
+root = tk.Tk()
+app = App("Class Vacancy System", master=root)
+app.master.title("Class Vacancy System")
+app.mainloop()
+root.destroy()
